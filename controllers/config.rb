280e142d1aaa4d8ed5c327b70f3491ca6de29254
Zachary Anker <zach.anker@gmail.com>
2012-12-05 18:56:49 -0800

    Rewrote the configuration page to support auto discovery of the Hub IP on initial configuration

--- controllers/config.rb
+++ controllers/config.rb
@@ -1,36 +1,32 @@
 class HueController < Sinatra::Base
   get "/config" do
-    @username = Digest::SHA1.hexdigest(rand(30 ** 30).to_s)
-
-    haml :config, :locals => {:action => self.config[:ip] ? "config-update" : "config-new", :no_action_css => true}
+    haml :config, :locals => {:action => "config", :no_action_css => true}
   end
 
   post "/config" do
-    url = URI("http://#{params[:ip]}/api")
+    self.save_config(:ip => params[:ip], :apikey => params[:username])
+    204
 
-    http = Net::HTTP.new(url.host, url.port)
-    res = http.request_post(url.request_uri, {:username => params[:username], :devicetype => params[:devicetype]}.to_json)
-    res = JSON.parse(res.body)
-
-    res = res.first
-    if res["error"] and res["error"]["type"] == 101
-      [400, "waiting"]
-    elsif res["error"]
-      [400, res["error"]["description"]]
-    elsif res["success"]
-      self.save_config(:ip => params[:ip], :apikey => params[:username])
-
-      [200, "success"]
-    end
+    #url = URI("http://#{params[:ip]}/api")
+    #
+    #http = Net::HTTP.new(url.host, url.port)
+    #res = http.request_post(url.request_uri, {:username => params[:username], :devicetype => params[:devicetype]}.to_json)
+    #res = JSON.parse(res.body)
+    #
+    #res = res.first
+    #if res["error"] and res["error"]["type"] == 101
+    #  [400, "waiting"]
+    #elsif res["error"]
+    #  [400, res["error"]["description"]]
+    #elsif res["success"]
+    #  self.save_config(:ip => params[:ip], :apikey => params[:username])
+    #
+    #  [200, "success"]
+    #end
   end
 
-  put "/config" do
-    self.config[:ip] = params[:ip]
-
-    File.open("./config/config.yml", "w+") do |f|
-      f.write(self.config.to_yaml)
-    end
-
-    [200, "success"]
+  delete "/config/apikey" do
+    self.save_config(:apikey => nil)
+    204
   end
 end
---
+++ public/js/controllers/config.js
@@ -0,0 +1,121 @@
+(function() {
+  // Reset the API Key we have saved so a new one is generated
+  $("#forget").click(function(event) {
+    event.preventDefault();
+    if( !confirm("Are you sure?") ) return;
+
+    $(this).button("loading");
+
+    $.ajax("/config/apikey", {
+      type: "DELETE",
+      complete: function() {
+        window.location.reload();
+      }
+    });
+  });
+
+  // Try and discover the IP of the hub
+  $("#discover").click(function(event) {
+    event.preventDefault();
+
+    $(this).button("loading");
+    $(".success, .error").removeClass("success").removeClass("error");
+
+    $.ajax("/discover", {
+      type: "POST",
+      error: function() {
+        $("#ip").closest(".controls").find(".help-inline").text("Timed out trying to find IP").addClass("error");
+        $("#discover").button("reset");
+      },
+      success: function(ip) {
+        $("#ip").val(ip).addClass("success");
+        $("#ip").closest(".controls").find(".help-inline").text("Found IP!").addClass("success");
+        $("#discover").button("reset");
+      }
+    });
+  });
+
+  var new_key;
+  if( $("#forget").length == 0 ) {
+    new_key = true;
+
+    // Force a discovery check when we load the page if we don't have an API Key set
+    if( $("#ip").val() == "" ) {
+      $("#discover").trigger("click");
+    }
+  }
+
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    Helper.reset_errors();
+    $(".success, .error").removeClass("success").removeClass("error");
+
+    var ip = $.trim($("#ip").val());
+    if( ip == "" ) {
+      return Helper.field_error("ip", "Please enter an IP");
+    }
+
+    var scope = $(this);
+    if( new_key ) {
+      var name = $.trim($("#devicetype").val());
+      if( name == "" ) {
+        return Helper.field_error("devicetype", "Please enter a name");
+      }
+
+      scope.find("input[type='submit']").button("auth");
+
+      var check_request = function() {
+        $.ajax("http://" + ip + "/api", {
+          type: "POST",
+          data: JSON.stringify({username: $("#username").val(), devicetype: name}),
+          error: function(res, textStatus, error) {
+            if( res.readyState == 0 ) return;
+
+            var text = "Failed to send request: " + textStatus;
+            if( typeof(error) == "string" && error != "" ) text += " (" + text + ")";
+
+            $("#error span").text(text).removeClass("hide");
+            $("#auth-modal").modal("hide");
+            scope.find("input[type='submit']").button("reset");
+          },
+          success: function(res) {
+            res = res[0];
+
+            // Still waiting
+            if( res.error && res.error.type == 101 ) {
+              setTimeout(check_request, 1000);
+
+            // Error, need to stop trying to authorize
+            } else if( res.error ) {
+              $("#error span").text(res.error.description).removeClass("hide");
+              $("#auth-modal").modal("hide");
+
+            // Authorized!
+            } else {
+              new_key = null;
+
+              $("#auth-modal").modal("hide");
+
+              scope.find("input[type='submit']").button("loading");
+              scope.submit();
+            }
+          }
+        });
+      };
+
+      $("#auth-modal").modal({backdrop: "static", keyboard: false});
+      check_request();
+      return;
+    }
+
+    $.ajax(scope.attr("action"), {
+      type: "POST",
+      data: {ip: ip, username: $("#username").val()},
+      success: function() {
+        $("#auth-modal").modal("hide");
+        window.location = "/";
+      }
+    });
+  });
+})();
