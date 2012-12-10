86ca71c48ac65670db52527413713936a4e88437
Zachary Anker <zach.anker@gmail.com>
2012-12-09 19:53:23 -0800

    Add error handling Add handling for job classes

--- worker/processor.rb
+++ worker/processor.rb
@@ -1,6 +1,6 @@
 module Worker
   class Processor
-    attr_reader :boss
+    attr_reader :boss, :state_mutex
 
     def initialize(boss, options)
       @boss = boss
@@ -18,9 +18,18 @@ module Worker
         @boss.job_states[job[:id]][:active] = true
       end
 
-      @threads << Thread.new(self, job) do |processor, job|
-        Worker::Job.new.run(processor.boss.job_states[job[:id]], job)
-        processor.job_finished(job)
+      @threads << Thread.new(self, job) do |processor, data|
+        begin
+          klass = Worker::Job.const_get(data[:type].capitalize)
+
+          states = klass.new(processor.state_mutex, processor.boss.job_states[data[:id]], job).run
+          @boss.communicator.apply_states(states)
+
+          processor.job_finished(data)
+        rescue => ex
+          puts "*** ERROR: #{ex.class}, #{ex.message}"
+          puts ex.backtrace
+        end
       end
 
       :active
@@ -32,11 +41,11 @@ module Worker
     end
 
     def job_finished(job)
-      puts "** Finished job #{job[:name]} (#{job[:id]})"
-
       @state_mutex.synchronize do
         state = @boss.job_states[job[:id]]
         state[:runs_left] -= 1
+
+        puts "** Finished job #{job[:name]} (#{state[:runs_left]} #{state[:runs_left] == 1 ? "run" : "runs"} left, id #{job[:id]})"
 
         # We're done with this job
         if state[:runs_left] <= 0
@@ -44,7 +53,8 @@ module Worker
 
         # Can run again
         else
-          state[:run_at] = Time.now.utc + (job[:transitiontime] * 0.10)
+          # Technically it's 0.10 not 0.12 but we want to futz it a bit so it doesn't instantly transition back
+          state[:run_at] = Time.now.utc + (job[:transitiontime] * 0.12)
           state[:active] = nil
         end
       end
