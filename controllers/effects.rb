d2827a103ac1370ceb8910867d2745a1e693cd57
Zachary Anker <zach.anker@gmail.com>
2012-12-09 16:59:48 -0800

    Add all of the front end for effects

---
+++ controllers/effects.rb
@@ -0,0 +1,55 @@
+class HueController < Sinatra::Base
+  get "/effects" do
+    haml :effects, :locals => {:action => "effects", :no_action_css => true}
+  end
+
+  post "/effect/pulse" do
+    effect = {}
+    effect[:name] = params[:name].to_s
+
+    if params[:lights].is_a?(Array)
+      effect[:lights] = params[:lights].map {|l| l.to_i}
+    end
+
+    if params[:groups].is_a?(Array)
+      effect[:groups] = params[:groups].map {|g| g.to_i}
+    end
+
+    effect[:times_to_run] = params[:times_to_run].to_i
+    effect[:transitiontime] = params[:transitiontime].to_i
+    effect[:start_color] = {:hue => params[:start_color][:h].to_i, :sat => params[:start_color][:s].to_i, :bri => params[:start_color][:v].to_i}
+    effect[:end_color] = {:hue => params[:end_color][:h].to_i, :sat => params[:end_color][:s].to_i, :bri => params[:end_color][:v].to_i}
+    effect[:alternate] = params[:alternate] == "true"
+    effect[:id] = Digest::SHA1.hexdigest("#{effect.to_s}#{Time.now.utc.to_f}")
+
+    if params[:save_set]
+      effect[:effect] = true
+
+      self.config[:sets] ||= {}
+      set_id = self.config[:sets].keys.max.to_i + 1
+
+      self.config[:sets][set_id] = effect
+      self.save_config(:sets => self.config[:sets])
+
+    else
+      self.update_jobs do
+        self.jobs.push(effect)
+      end
+    end
+
+    204
+  end
+
+  delete "/effect/:id" do
+    self.update_jobs do
+      self.jobs.delete_if {|v| v[:id] == params[:id]}
+    end
+
+    204
+  end
+
+  delete "/effects" do
+    self.update_jobs(false) { self.jobs.clear }
+    204
+  end
+end
---
+++ public/js/controllers/effects.js
@@ -0,0 +1,88 @@
+(function() {
+  $(".tabbable ul li a").click(function(event) {
+      event.preventDefault();
+      $(this).tab("show");
+  });
+
+  $("#stop-all").click(function(event) {
+    event.preventDefault();
+    $(this).button("loading");
+
+    $.ajax("/effects", {type: "DELETE", complete: function() { window.location.reload(); }});
+  });
+
+  var save_set;
+  $("#save-set").click(function(event) {
+    event.preventDefault();
+
+    save_set = true;
+    $("form").submit();
+    save_set = false;
+  });
+
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var lights = $("#lights").val();
+    var groups = $("#groups").val();
+
+    if( ( !lights || lights.length == 0 ) && ( !groups || groups.length == 0 ) ) {
+      return Helper.error("You must select at least one light or group.");
+    }
+
+    var name = $.trim($("#name").val());
+    if( name == "" ) {
+      return Helper.error("You must enter a name.");
+    }
+
+    var scope = $(this);
+
+    var data = {};
+    data.save_set = save_set;
+    data.name = name;
+    data.lights = lights;
+    data.groups = groups;
+    data.times_to_run = parseInt($("#times_to_run").val()) || 1;
+    data.transitiontime = parseInt($("#transitiontime").val()) || 10;
+
+    if( data.transitiontime <= 0 ) {
+      return Helper.field_error("transitiontime", "Cannot be below 0.");
+    } else if( data.times_to_run <= 0 ) {
+      return Helper.field_error("times_to_run", "Cannot be below 0.");
+    }
+
+    Helper.reset_errors();
+    scope.find(data.save_set ? "input[type='button']" : "input[type='submit']").button("loading");
+    scope.find("input[type='button'], input[type='submit']").attr("disabled", true);
+
+    // Find any specific settings to the effect ytpe
+    scope.find(".tab-pane.active").find("input[type='number'], input[type='text'], select").each(function() {
+      var field = $(this);
+
+      var val;
+      if( field.hasClass("colorpicker") ) {
+        val = Helper.hex_to_hsv(field.val());
+      } else {
+        val = field.val();
+      }
+
+      data[field.attr("id")] = val;
+    });
+
+    $.ajax("/effect/" + scope.find(".tab-pane.active").attr("id"), {
+      type: "POST",
+      data: data,
+      success: function() {
+        scope.find(data.save_set ? "input[type='button']" : "input[type='submit']").button("reset");
+        scope.find("input[type='button'], input[type='submit']").attr("disabled", true);
+
+        if( data.save_set ) {
+          $("#finished .modal-body").html("<p class='text-success'>Saved new effect as a set named \"" + data.name + "\".</p>");
+        } else {
+          $("#finished .modal-body").html("<p class='text-success'>Starting effect. This may take up to 10 - 20 seconds to start.</p><p>If nothing happens, make sure the worker is enabled.</p>");
+        }
+
+        $("#finished").modal();
+      }
+    });
+  });
+})();
