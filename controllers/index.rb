c1d5ecf30148cfdd36ef1c2c30d902584558f0ce
Zachary Anker <zach.anker@gmail.com>
2012-12-13 08:05:24 -0800

    Make sure we redirect to the configuration page if none is set yet

--- controllers/index.rb
+++ controllers/index.rb
@@ -1,5 +1,9 @@
 class HueController < Sinatra::Base
   get "/" do
+    unless self.config[:apikey]
+      return redirect("/config")
+    end
+
     haml :layout, :layout => false, :locals => {:action => :index}
   end
 end
